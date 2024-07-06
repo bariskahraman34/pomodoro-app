467bb7a77e2bb9522e6165ab82ec65c5b3eb8134
Erhan Yaşar <mail@erhanyasar.com.tr>
2024-07-06 15:13:48 +0300

    feat: adds mui theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -7,7 +8,9 @@ import { GlobalStateProvider } from './store/globalState/provider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStateProvider>
-      <App />
+      <ThemeProvider theme={createTheme()}>
+        <App />
+      </ThemeProvider>
     </GlobalStateProvider>
   </React.StrictMode>,
 )
