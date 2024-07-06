74782e88cbbc7d27ee5255c4b574a2e04c7beca8
Erhan Yaşar <mail@erhanyasar.com.tr>
2024-07-06 15:14:42 +0300

    feat: adds all components into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-function App() {
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Box, Container, Stack, Typography } from '@mui/material';
+import { CustomTabs } from './components/tabs';
+import { Timer } from './components/timer';
 
+function App() {
   return (
-    <></>
+    <Container maxWidth="lg">
+      <Box sx={{flexGrow: 1}}>
+        <Stack>
+          <Typography variant='h6' component="h2">pomodoro</Typography>
+          <CustomTabs />
+          <Timer />
+          <SettingsIcon />
+        </Stack>
+      </Box>
+    </Container>
   )
 }
 
