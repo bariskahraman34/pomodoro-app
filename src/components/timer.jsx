a2464a51a540afa710721f3a8501943676f06889
Erhan Yaşar <mail@erhanyasar.com.tr>
2024-07-06 16:59:33 +0300

    feat: creates and adds timer component

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,5 +1,40 @@
+import { Button, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+
 export const Timer = () => {
-    return (<>
-        Timer
-    </>)
+    const [remainingTime, setRemainingTime] = useState("17:59");
+
+    return (
+        <Stack
+            sx={{
+                width: "410px",
+                height: "410px",
+                borderRadius: '50%',
+                boxShadow: "50px 50px 100px 0px #121530",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
+        >
+            <Stack
+                sx={{
+                    width: "366px",
+                    height: "366px",
+                    borderRadius: '50%',
+                    backgroundColor: '#161932',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    // boxShadow: "-50px -50px 100px 0px #272C5A",
+                }}
+            >
+                <Typography variant='h1' component="h2">{remainingTime}</Typography>
+                <Button
+                    variant="text" 
+                    sx={{color:'white', fontSize: '18px'}}
+                    onClick={() => {}}
+                >PAUSE</Button>
+            </Stack>
+        </Stack>
+    )
 }
