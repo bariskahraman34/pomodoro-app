174fa7e3809c4e2223f9943d33b03504530a4d8f
Barış Kahraman <baris-kahra@hotmail.com>
2024-07-09 01:20:43 +0300

    feat: Add initial values for color mode and start time

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,4 +1,13 @@
 export const initialState = {
     counter: 0,
     isLoggedIn: false,
+    colorMode: "#1976d2",
+    time: {
+        pomodoro:"25:00",
+        shortBreak: "05:00",
+        longBreak: "15:00"
+    },
+    currentTime: 0,
+    isStarted:false,
+    isFinished: false
 }
